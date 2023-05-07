b99322e2d5f5f70f75ad6c4332836982970558f1
haimavatinandanpati <122ee0607@nitrkl.ac.in>
2023-05-07 13:28:51 +0530

    first commit

--- simple calculator.py
+++ simple calculator.py
@@ -10,24 +10,25 @@ def multiply(x, y):
 def divide(x, y):
     return x / y
 
-print("Select an operation:")
-print("1. Addition")
-print("2. Subtraction")
-print("3. Multiplication")
-print("4. Division")
-
-choice = input("Enter your choice (1/2/3/4): ")
-
 num1 = float(input("Enter the first number: "))
+
+print("Select an operation:")
+print("+ Addition")
+print("- Subtraction")
+print("* Multiplication")
+print("/ Division")
+
+choice = input("Enter your choice : ")
+
 num2 = float(input("Enter the second number: "))
 
-if choice == '1':
+if choice == '+':
     print(num1, "+", num2, "=", add(num1, num2))
-elif choice == '2':
+elif choice == '-':
     print(num1, "-", num2, "=", subtract(num1, num2))
-elif choice == '3':
+elif choice == '*':
     print(num1, "*", num2, "=", multiply(num1, num2))
-elif choice == '4':
+elif choice == '/':
     if num2 != 0:
         print(num1, "/", num2, "=", divide(num1, num2))
     else:
