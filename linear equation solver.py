70c3320d5603b8f53b50ec41ea9cc401598ae4a4
haimavatinandanpati <122ee0607@nitrkl.ac.in>
2023-05-09 21:45:18 +0530

    improved version

---
+++ linear equation solver.py
@@ -0,0 +1,24 @@
+def solve_linear_equation(a1, b1, c1, a2, b2, c2):
+    determinant = a1 * b2 - a2 * b1
+
+    if determinant == 0:
+        # The system has either no solution or infinitely many solutions
+        if c1 == c2:
+            print("The system has infinitely many solutions.")
+        else:
+            print("The system has no solution.")
+    else:
+        x = (b2 * c1 - b1 * c2) / determinant
+        y = (a1 * c2 - a2 * c1) / determinant
+        print("The solution is x =", x, "and y =", y)
+
+
+# Example usage
+a1 = 2
+b1 = -3
+c1 = 7
+a2 = 4
+b2 = 1
+c2 = 9
+
+solve_linear_equation(a1, b1, c1, a2, b2, c2)
